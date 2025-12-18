acc8dbd0772a3420b0816dd1beff362561a2c3b0
irajverma <i.rajverma8423@gmail.com>
2025-12-18 18:17:50 +0530

    changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, // Increases the limit to 1000kB (1MB)
-  },
 })
